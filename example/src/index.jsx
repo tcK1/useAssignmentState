b9e272f4f2af56db58e6f55ae1562b5be3d39a31
Kaic Bastidas <kaicbastidas@gmail.com>
2019-07-27 22:19:21 -0300

    feat: first working version

--- example/src/index.jsx
+++ example/src/index.jsx
@@ -1,15 +1,88 @@
-import React from 'react';
+import React, { useMemo } from 'react';
 import ReactDOM from 'react-dom';
 
-import useMyHook from 'use-assignment-state';
+import useAssignmentState from 'use-assignment-state';
 
 const App = () => {
-  const [a, setA] = useMyHook();
+  const obj1 = useAssignmentState({
+    a: 1,
+    b: 2,
+    c: [1, 2, 3, 4],
+    d: {
+      eita: 'hmm',
+      mais: {
+        fundo: 'kkk',
+      },
+    },
+  });
+  const obj2 = useAssignmentState({ a: 1, c: 3 });
+
+  const sum1 = useMemo(() => {
+    console.log('sum1');
+    return (obj1.a + obj1.b);
+  }, [obj1.a, obj1.b]);
+
+  const sum2 = useMemo(() => {
+    console.log('sum2');
+    return (obj2.a + obj2.c);
+  }, [obj2.a, obj2.c]);
 
   return (
-    <button onClick={() => setA(a + 1)} type="button">
-      {a}
-    </button>
+    <div>
+      <pre>
+        <code>
+          {JSON.stringify(obj1, undefined, 2)}
+        </code>
+      </pre>
+      <div>
+        obj1 sum:
+        {' '}
+        {sum1}
+      </div>
+
+      <button onClick={() => { obj1.a += 1; }} type="button">
+        {obj1.a}
+      </button>
+      <button onClick={() => { obj1.b += 1; }} type="button">
+        {obj1.b}
+      </button>
+      <button onClick={() => { obj1.c[0] += 1; }} type="button">
+        {obj1.c.map(val => val)}
+      </button>
+      <button onClick={() => { obj1.c.reverse(); }} type="button">
+        {obj1.c.map(val => val)}
+      </button>
+      <button onClick={() => { obj1.c = ['outro', 'array']; }} type="button">
+        {obj1.c.map(val => val)}
+      </button>
+      <button onClick={() => { obj1.d.eita = 'eita'; }} type="button">
+        {obj1.d.eita}
+      </button>
+      <button onClick={() => { obj1.d.mais.fundo += 'k'; }} type="button">
+        {obj1.d.mais.fundo}
+      </button>
+      <button onClick={() => { obj1.d.mais = { outro: 'vix' }; }} type="button">
+        {obj1.d.mais.fundo}
+      </button>
+      <button onClick={() => { obj1.e = [1, 2]; }} type="button">
+        {obj1.e}
+      </button>
+
+      <br />
+
+      <div>
+        obj2 sum:
+        {' '}
+        {sum2}
+      </div>
+
+      <button onClick={() => { obj2.a += 1; }} type="button">
+        {obj2.a}
+      </button>
+      <button onClick={() => { obj2.c += 1; }} type="button">
+        {obj2.c}
+      </button>
+    </div>
   );
 };
 
--- src/index.js
+++ src/index.js
@@ -1,9 +1,34 @@
-import { useState } from 'react';
+import { useMemo, useReducer } from 'react';
 
-const useMyHook = () => {
-  const [a, setA] = useState(1);
+const handler = setState => ({
+  get: (target, prop) => {
+    if (typeof target[prop] === 'object' && target[prop] !== null) {
+      return new Proxy(target[prop], handler(setState));
+    }
 
-  return [a, setA];
+    return target[prop];
+  },
+  set: (obj, prop, value) => {
+    if (obj[prop] !== value) {
+      setState({ [prop]: value });
+      /* eslint-disable-next-line no-param-reassign */
+      obj[prop] = value;
+    }
+    return true;
+  },
+});
+
+const useAssignmentState = (initialValue) => {
+  const reducer = (state, newState) => ({ ...state, ...newState });
+  /* eslint-disable-next-line no-unused-vars */
+  const [state, setState] = useReducer(
+    reducer,
+    initialValue,
+  );
+
+  const object = useMemo(() => new Proxy(initialValue, handler(setState)), []);
+
+  return object;
 };
 
-export default useMyHook;
+export default useAssignmentState;
